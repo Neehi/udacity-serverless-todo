79f840253881ffe765bcb43223d2450c86b83b7d
Neehi <nick.snape@hotmail.com>
2020-04-17 00:22:52 +0100

    Update generateUploadUrl to save attachment url

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -2,25 +2,44 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
+
+import { getUserId } from '../utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
 
 const s3 = new AWS.S3({
   signatureVersion: 'v4'
 })
 
+const todosTable = process.env.TODOS_TABLE
 const bucketName = process.env.ATTACHMENTS_S3_BUCKET
 const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event)
 
+  const userId = getUserId(event)
   const todoId = event.pathParameters.todoId
+  const attachmentId = uuid.v4()
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
   const uploadUrl = s3.getSignedUrl('putObject', {
     Bucket: bucketName,
-    Key: todoId,
+    Key: attachmentId,
     Expires: urlExpiration
   })
+
+  await docClient.update({
+    TableName: todosTable,
+    Key: {
+      userId,
+      todoId
+    },
+    UpdateExpression: 'set attachmentUrl = :attachmentUrl',
+    ExpressionAttributeValues: {
+      ':attachmentUrl': `https://${bucketName}.s3.amazonaws.com/${attachmentId}`
+    }
+  }).promise();
 
   return {
     statusCode: 200,
