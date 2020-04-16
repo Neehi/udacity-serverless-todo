a8e50d94fdf806ba1301b509dce45f14e84fa5db
Neehi <nick.snape@hotmail.com>
2020-04-16 20:26:10 +0100

    Fix bug returning new item

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -41,7 +41,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
-      newItem
+      item: newItem
     })
   }
 }
