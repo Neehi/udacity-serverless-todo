fb9aaa2fa9f9e6403372e96161b10d7517269636
Neehi <nick.snape@hotmail.com>
2020-04-16 12:42:57 +0100

    Implement Auth0 authentication

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,10 +9,9 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-cr9isr4f.eu.auth0.com/.well-known/jwks.json'
+
+let cachedCertificate: string
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +57,9 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const cert = await getCertificate()
+
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +72,23 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getCertificate(): Promise<string> {
+  if (cachedCertificate) return cachedCertificate
+
+  logger.info('Fetching certificate', jwksUrl)
+
+  const response = await Axios.get(jwksUrl);
+  const key = response.data.keys[0]  // XXX: Handle multiple keys?
+
+  if (!key || key.use !== 'sig' || key.kty !== 'RSA' || key.alg !== 'RS256' || !key.kid || !key.x5c || !key.x5c.length) {
+    throw new Error('Invalid certificate')
+  }
+
+  cachedCertificate = `-----BEGIN CERTIFICATE-----\n${key.x5c[0]}\n-----END CERTIFICATE-----`;  
+
+  logger.info('Valid certificate found', cachedCertificate)
+
+  return cachedCertificate
 }
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'i584i3b0cj'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-cr9isr4f.eu.auth0.com',           // Auth0 domain
+  clientId: 'so2GI2QmQaak1RzPnksGTsbsX1Nx9Oxv',  // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
